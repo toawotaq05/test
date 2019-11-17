554f89f1f7ba83ca39a74336b02fe4652999ba86
toawotaq05 <cr0ntoz@gmail.com>
2019-11-17 20:49:06 +0100

    ett första test

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print('hello world')
+
+17+25
+test = pd.DataFrame({'col1': [1,2], 'col2': [3,4]})
+
+N = 50
+x=np.random.rand(N)
+y=np.random.rand(N)
+colors = np.random.rand(N)
+area= (30*np.random.rand(N))**2
+plt.scatter(x,y,s=area, c=colors, alpha=0.5)
+plt.show()
+test
